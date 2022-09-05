740e0387a0c8e4f407124430d93e77efa1cc461e
immadisairaj <immadirajendra.sai@gmail.com>
2022-09-05 20:05:06 +0530

    fix license of blogpost

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -11,14 +11,16 @@ export default function BlogPostItemWrapper(props) {
   return (
     <>
       <BlogPostItem {...props} />
-      <div class="license-wrapper
-      d-flex justify-content-between align-items-center pt-2 pb-2">
-        This post is licensed under
-        &nbsp;
-        <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
-        &nbsp;
-        by the author.
-      </div>
+      {comments && (
+        <div class="license-wrapper
+        d-flex justify-content-between align-items-center pt-2 pb-2">
+          This post is licensed under
+          &nbsp;
+          <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
+          &nbsp;
+          by the author.
+        </div>
+      )}
       {comments && (
         <DiscussionEmbed
           shortname='immadisairaj-github-io'
